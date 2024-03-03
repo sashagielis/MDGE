7af3247b92b3177ed8917ed58fdba6a59c5ae40d
Sasha Gielis <s.gielis@student.tue.nl>
2024-03-03 22:10:02 +0100

    Refactor code and add methods to check for segment intersection

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import os
-import xml.etree.ElementTree as ET
-
-from graph import *
-from obstacle import *
-from utils import transform_point
+from input_parser import read_ipe_instance
 from visualizer import visualize
 
 
@@ -15,71 +10,6 @@ ipe_instances = {
         'instance_from_report',
     ]
 }
-
-
-def read_ipe_instance(instance):
-    vertices = []
-    edges = []
-    obstacles = []
-
-    tree = ET.parse(f'instances/ipe/{instance}')
-    root = tree.getroot()
-
-    current_layer = None
-    for obj in root.find('./page'):
-        if obj.tag not in ['use', 'path']:
-            continue
-
-        if obj.get('layer') is not None:
-            current_layer = obj.get('layer').casefold()
-
-        if obj.get('matrix') is not None:
-            transformation = [float(i) for i in obj.get('matrix').split()]
-        else:
-            transformation = [1, 0, 0, 1, 0, 0]
-
-        match obj.tag:
-            case 'use':
-                pos = [float(i) for i in obj.get('pos').split()]
-                point = Point(pos[0], pos[1])
-                transform_point(point, transformation)
-
-                color = obj.get('stroke')
-
-                if current_layer == 'graph':
-                    vertex = Vertex(point.x, point.y, color)
-                    vertices.append(vertex)
-                else:
-                    obstacle = Obstacle([point], color)
-                    obstacles.append(obstacle)
-
-            case 'path':
-                nodes_string = os.linesep.join([s for s in obj.text.splitlines() if s])
-                path = []
-                for node_string in nodes_string.split('\n'):
-                    coordinates = [float(i) for i in node_string.split()[:-1]]
-                    point = Point(coordinates[-2], coordinates[-1])
-                    transform_point(point, transformation)
-                    path.append(point)
-
-                if current_layer == 'graph':
-                    if obj.get('custom') is not None:
-                        weight = obj.get('custom')
-                    else:
-                        weight = 1
-
-                    edge = Edge(path, weight, obj.get('stroke'))
-                    edges.append(edge)
-                else:
-                    if len(path) > 1:
-                        path = path[:-1]
-
-                    obstacle = Obstacle(path, obj.get('fill'), obj.get('stroke'))
-                    obstacles.append(obstacle)
-
-    graph = Graph(vertices, edges)
-
-    return graph, obstacles
 
 
 def main():
--- utils.py
+++ utils.py
@@ -11,18 +11,83 @@ def transform_point(p, t):
     p.y = t[2] * p.x + t[3] * p.y + t[5]
 
 
-def distance(p1, p2):
+def distance(p, q):
     """
-    Determines the distance between points p1 and p2.
+    Determines the distance between points p and q.
     """
-    return math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2)
+    return math.sqrt((p.x - q.x) ** 2 + (p.y - q.y) ** 2)
 
 
-def angle(p1, p2):
+def angle(p, q):
     """
-    Determines the angle of rotation of line segment (p1, p2).
+    Determines the angle of rotation of line segment pq.
     """
-    dx = p2.x - p1.x
-    dy = p2.y - p1.y
+    dx = q.x - p.x
+    dy = q.y - p.y
 
     return math.atan2(dy, dx)
+
+
+def on_segment(p, q, r):
+    """
+    Determines whether point r lies on line segment pq.
+    """
+    if max(p.x, q.x) >= r.x >= min(p.x, q.x) and max(p.y, q.y) >= r.y >= min(p.y, q.y):
+        return True
+
+    return False
+
+
+def orientation(p, q, r):
+    """
+    Determines the orientation of the ordered point triplet (p, q, r).
+    Source: https://www.geeksforgeeks.org/orientation-3-ordered-points/amp/
+    :returns 0 : collinear, 1 : clockwise, 2 : counterclockwise
+    """
+    val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y)
+
+    if val > 0:
+        # Clockwise orientation
+        return 1
+    elif val < 0:
+        # Counterclockwise orientation
+        return 2
+    else:
+        # Collinear orientation
+        return 0
+
+
+def do_intersect(p1, q1, p2, q2):
+    """
+    Determines whether line segments p1q1 and p2q2 intersect.
+    Source: https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
+    """
+    # Find the four orientations required for the general and special cases
+    o1 = orientation(p1, q1, p2)
+    o2 = orientation(p1, q1, q2)
+    o3 = orientation(p2, q2, p1)
+    o4 = orientation(p2, q2, q1)
+
+    # General case
+    if o1 != o2 and o3 != o4:
+        return True
+
+    # Special cases
+    # p1, q1 and p2 are collinear and p2 lies on segment p1q1
+    if o1 == 0 and on_segment(p1, q1, p2):
+        return True
+
+    # p1, q1 and q2 are collinear and q2 lies on segment p1q1
+    if o2 == 0 and on_segment(p1, q1, q2):
+        return True
+
+    # p2, q2 and p1 are collinear and p1 lies on segment p2q2
+    if o3 == 0 and on_segment(p2, q2, p1):
+        return True
+
+    # p2, q2 and q1 are collinear and q1 lies on segment p2q2
+    if o4 == 0 and on_segment(p2, q2, q1):
+        return True
+
+    # If none of the cases apply
+    return False
